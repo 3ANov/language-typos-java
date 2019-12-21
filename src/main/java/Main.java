067fc14ea1d9bb47e47bb0391e997237c5494285
3ANov <jeims0007@gmail.com>
2019-12-21 19:22:57 +0300

    Пробуем сделать всё со словарями

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,61 @@
-
+import org.json.JSONObject;
 import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) {
+
+        LangTypos converter = new LangTypos();
+
+        Map<String,String> dict = new HashMap<>();
+
+
+
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(ClassLoader.getSystemClassLoader()
+                        .getResourceAsStream("russian.txt"))))) {
+
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                dict.put(line,converter.convertLayout(line,"ru","en"));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        JSONObject jsonDict = new JSONObject(dict); // json
+
+
+        try (FileWriter file = new FileWriter("dictRuToEn.json",false)) {
+
+            file.write(jsonDict.toString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("dictRuToEn.json"))) {
+
+
+            String response = reader.lines().collect(Collectors.joining());
+
+
+            JSONObject jObject = new JSONObject(response); // json
+            System.out.println(jObject.toMap().get("оренбургская"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
 
 /*
